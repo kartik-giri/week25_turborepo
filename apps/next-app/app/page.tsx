0505997705b44a6991798a862b2e509477eff4e1
kartik-giri <kartikgiri1t30@gmail.com>
2026-06-05 22:29:31 +0530

    cd in to next app

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -1,13 +1,13 @@
 import { prisma } from "@repo/db"
 
 const Home = async ()=>{
-  // const user = await prisma.user.findFirst()
+  const user = await prisma.user.findFirst()
   return (
     <>
-    <h1>Development fforce on EC2 VM.</h1>
+    <h1>Development force on EC2 VM.</h1>
     {/* ? option chaning means if user exist render user if null or undefined just redner undefined */}
-    {/* <h1>Namee: {user?.username}</h1> 
-    <h1>Password : {user?.email}</h1> */}
+    <h1>Namee: {user?.username}</h1> 
+    <h1>Password : {user?.email}</h1>
     </>
   )
 }
