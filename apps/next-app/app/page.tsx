c274888da6ab6ee02b6af4fec8d3bef57ced9a86
kartik-giri <kartikgiri1t30@gmail.com>
2026-06-05 20:00:54 +0530

    edited app page

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -4,6 +4,7 @@ const Home = async ()=>{
   const user = await prisma.user.findFirst()
   return (
     <>
+    <h1>Development EC2 VM.</h1>
     {/* ? option chaning means if user exist render user if null or undefined just redner undefined */}
     <h1>{user?.username}</h1> 
     <h1>{user?.email}</h1>
