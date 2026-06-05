2b59918da0b829e644019a779846ab79264d8387
kartik-giri <kartikgiri1t30@gmail.com>
2026-06-05 21:57:04 +0530

    updated next app

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -6,7 +6,7 @@ const Home = async ()=>{
     <>
     <h1>Development EC2 VM.</h1>
     {/* ? option chaning means if user exist render user if null or undefined just redner undefined */}
-    <h1>Name: {user?.username}</h1> 
+    <h1>Namee: {user?.username}</h1> 
     <h1>Password: {user?.email}</h1>
     </>
   )
