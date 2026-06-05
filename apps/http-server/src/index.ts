d96d5bda950f634afcb665e9e85ab5801ebac82f
kartik-giri <kartikgiri1t30@gmail.com>
2026-06-05 20:10:00 +0530

    updated http-server

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -8,7 +8,7 @@ app.use(express.json())
 app.get("/", (req, res)=>{
     
     res.status(200).json({
-        message: "You are getting response back from http server"
+        message: "You are getting response back from updated http server using CD"
     })
 });
 
